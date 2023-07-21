0fe32c4a1a4717ec2865f34a6ccc9f96af0cca53
pawansikarwar0207 <pawansikarwar96@gmail.com>
2023-07-21 14:04:12 +0530

    add the tab for searching country & add the update the user detail

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,12 @@
 class HomeController < ApplicationController
   def index
     @q = User.ransack(params[:q])
-    @users = @q.result(distinct: true).order(created_at: :asc).limit(16)
+    if params[:country].present?
+      @users = User.where(country: params[:country]).order(created_at: :asc).limit(16)
+    else
+      @users = @q.result(distinct: true).order(created_at: :asc).limit(16)
+    end
   end
+
+
 end
--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,5 +1,31 @@
 class MembersController < ApplicationController
+
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+    respond_to do |format|
+      format.js
+      format.html
+    end
+  end
+
+  def update
+  @user = User.find(params[:id])
+  if @user.update(user_params)
+    redirect_to root_path, notice: 'User was successfully updated.'
+  else
+    render :edit
+  end
+end
+
+
+private
+
+def user_params
+  params.require(:user).permit(:first_name, :last_name, :date_of_birth, :about, :contact_number, :email, :username, :city, :state, :country, :pincode, :street_address, :profile_title)
+end
+
 end
