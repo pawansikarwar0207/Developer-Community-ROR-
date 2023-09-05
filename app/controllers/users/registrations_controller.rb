111e30c07b7454febf66c27c224e055cf47525ff
pawansikarwar0207 <pawansikarwar96@gmail.com>
2023-09-05 14:28:54 +0530

    customize the devise form

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -42,7 +42,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :username, :profile_title, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :username, :profile_title, :password, :password_confirmation, :image])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
