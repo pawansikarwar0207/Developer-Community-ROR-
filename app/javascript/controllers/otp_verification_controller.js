6d41a162bd64d8faebd2359206208e8f38f4f27b
pawansikarwar0207 <pawansikarwar96@gmail.com>
2023-09-25 18:14:00 +0530

    add the stimulus otp_verficiation for the otp & password

--- app/javascript/controllers/otp_verification_controller.js
+++ app/javascript/controllers/otp_verification_controller.js
@@ -2,9 +2,30 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="otp-verification"
 export default class extends Controller {
+  static targets = ["passwordField"];
 
   connect() {
     console.log("OTP Verification method")
+    this.hidePasswordField();
   }
 
+  showPasswordField() {
+    this.passwordFieldTarget.style.display = "block";
+  }
+
+  hidePasswordField() {
+    this.passwordFieldTarget.style.display = "none";
+  }
+
+  onOtpChange(event) {
+    const otpValue = event.target.value;
+    if (otpValue.length === 6) {
+      this.showPasswordField();
+      // Trigger a Turbo Stream to replace the password field div
+      const frame = document.querySelector("[data-turbo-frame='password-frame']");
+      TurboStreams.visit(frame.getAttribute("id"));
+    } else {
+      this.hidePasswordField();
+    }
+  }
 }
