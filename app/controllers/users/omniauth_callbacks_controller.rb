9aeedc3ea249125b4631ea17e103360c4b1582ec
pawansikarwar0207 <pawansikarwar96@gmail.com>
2023-09-27 19:15:05 +0530

    add the feature of login using OTP

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,14 +1,8 @@
 # frozen_string_literal: true
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  # You should configure your model like this:
-  # devise :omniauthable, omniauth_providers: [:twitter]
 
-  # You should also create an action method in this controller like this:
-  # def twitter
-  # end
-
-  def google_oauth2
+ def google_oauth2
    handle_auth "Google"
  end
 
@@ -18,37 +12,16 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
 
  def handle_auth(kind)
-  # You need to implement the method below in your model (e.g. app/models/user.rb)
-  @user = User.from_omniauth(request.env['omniauth.auth'])
+    # You need to implement the method below in your model (e.g. app/models/user.rb)
+    @user = User.from_omniauth(request.env['omniauth.auth'])
 
-  if @user.persisted?
-    flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: kind
-    sign_in_and_redirect @user, event: :authentication
-  else
-    session['devise.auth_data'] = request.env['omniauth.auth'].except('extra') # Removing extra as it can overflow some session stores
-    redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+    if @user.persisted?
+      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: kind
+      sign_in_and_redirect @user, event: :authentication
+    else
+      session['devise.auth_data'] = request.env['omniauth.auth'].except('extra') # Removing extra as it can overflow some session stores
+      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+    end
   end
-end
 
-  # More info at:
-  # https://github.com/heartcombo/devise#omniauth
-
-  # GET|POST /resource/auth/twitter
-  # def passthru
-  #   super
-  # end
-
-  # GET|POST /users/auth/twitter/callback
-  # def failure
-  #   super
-  # end
-
-  # protected
-
-  # The path used when OmniAuth fails
-  # def after_omniauth_failure_path_for(scope)
-  #   super(scope)
-  # end
-
-  
 end
