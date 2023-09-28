6c0ef3049af0bda7070800bcdc05b8188f40ebcd
pawansikarwar0207 <pawansikarwar96@gmail.com>
2023-09-28 17:02:48 +0530

    fix the bugs

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,14 +15,13 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = Post.create(post_params)
     if @post.save
       redirect_to root_path
     else
       render :new
     end
   end
-
 
   def show
     @user = @post.user
